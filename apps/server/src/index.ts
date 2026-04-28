931e3af71477a7b3dbb3d79f3f9d0259704307e6
Vinícius Estrela <guimaraesviniciusestrela@gmail.com>
2026-04-28 13:22:16 -0300

    initial commit

---
+++ apps/server/src/index.ts
@@ -0,0 +1,95 @@
+import { devToolsMiddleware } from "@ai-sdk/devtools";
+import { google } from "@ai-sdk/google";
+import { cors } from "@elysiajs/cors";
+import { createContext } from "@obras360-project/api/context";
+import { appRouter } from "@obras360-project/api/routers/index";
+import { auth } from "@obras360-project/auth";
+import { env } from "@obras360-project/env/server";
+import { OpenAPIHandler } from "@orpc/openapi/fetch";
+import { OpenAPIReferencePlugin } from "@orpc/openapi/plugins";
+import { onError } from "@orpc/server";
+import { RPCHandler } from "@orpc/server/fetch";
+import { ZodToJsonSchemaConverter } from "@orpc/zod/zod4";
+import { convertToModelMessages, streamText, wrapLanguageModel } from "ai";
+import { Elysia } from "elysia";
+
+const rpcHandler = new RPCHandler(appRouter, {
+  interceptors: [
+    onError((error) => {
+      console.error(error);
+    }),
+  ],
+});
+const apiHandler = new OpenAPIHandler(appRouter, {
+  plugins: [
+    new OpenAPIReferencePlugin({
+      schemaConverters: [new ZodToJsonSchemaConverter()],
+    }),
+  ],
+  interceptors: [
+    onError((error) => {
+      console.error(error);
+    }),
+  ],
+});
+
+const app = new Elysia()
+  .use(
+    cors({
+      origin: env.CORS_ORIGIN,
+      methods: ["GET", "POST", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+      credentials: true,
+    }),
+  )
+  .all("/api/auth/*", async (context) => {
+    const { request, status } = context;
+    if (["POST", "GET"].includes(request.method)) {
+      return auth.handler(request);
+    }
+    return status(405);
+  })
+  .all(
+    "/rpc*",
+    async (context) => {
+      const { response } = await rpcHandler.handle(context.request, {
+        prefix: "/rpc",
+        context: await createContext({ context }),
+      });
+      return response ?? new Response("Not Found", { status: 404 });
+    },
+    {
+      parse: "none",
+    },
+  )
+  .all(
+    "/api-reference*",
+    async (context) => {
+      const { response } = await apiHandler.handle(context.request, {
+        prefix: "/api-reference",
+        context: await createContext({ context }),
+      });
+      return response ?? new Response("Not Found", { status: 404 });
+    },
+    {
+      parse: "none",
+    },
+  )
+  .post("/ai", async (context) => {
+    const body = await context.request.json();
+    const uiMessages = body.messages || [];
+    const model = wrapLanguageModel({
+      model: google("gemini-2.5-flash"),
+      middleware: devToolsMiddleware(),
+    });
+    const result = streamText({
+      model,
+      messages: await convertToModelMessages(uiMessages),
+    });
+
+    return result.toUIMessageStreamResponse();
+  })
+  .get("/", () => "OK")
+  .listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+  });
